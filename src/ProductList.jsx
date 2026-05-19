48eed3940c9e9c5b24b14df10a93d9a3d3c213db
Yimmy456 <manarajhar@hotmail.com>
2026-05-19 09:16:34 -0400

    Initial Commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -274,7 +274,34 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
+                    {plantsArray.map((category, index) => (
+                        <div key={index}>
+                            <h1>
+                                <div>{category.category}</div>
+                            </h1>
+                            <div className="product-list">
+                                {category.plants.map((plant, plantIndex) =>(
+                                    <div className="product-card" key={plantIndex}>
+                                        <img
+                                        className="product-name"
+                                        src={plant.image}
+                                        alt={plant.name}
+                                        />
+                                        <div className="product-title">{plant.name}</div>
+                                        <div className="product-description">{ProductList.description}</div>
+                                        <div className="product-cost">${plant.cost}</div>
+                                        <button
+                                            className="product-button"
+                                            onClick={() => handleAddToCart(plant)}
+                                        >
+                                            Add to Cart
+                                        </button>
 
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
 
                 </div>
             ) : (
