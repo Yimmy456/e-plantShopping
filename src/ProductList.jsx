bab744fd625a3e2cc963c9985c8f80c36444db84
Yimmy456 <manarajhar@hotmail.com>
2026-05-19 10:10:16 -0400

    Adding, Removing, and Updating Items

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice'
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+
+    const [addedToCart, setAddedToCart] = useState({});
+
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+
+        setAddedToCart((prevState) => ({
+            ...prevState,
+            [product.name]: true,
+        }));
+    };
 
     const plantsArray = [
         {
